a6a206019492f01df51be622a5305b261f75b927
B1500User <adam.micolich@gmail.com>
2024-06-25 11:55:50 +1200

    B1500 Comms success midday 25Jun24

--- PyNE-kiwi/test.py
+++ PyNE-kiwi/test.py
@@ -16,8 +16,13 @@ print(ResList)
 B2201A = rm.open_resource('GPIB0::22::INSTR')
 print(B2201A.query("*IDN?"))
 
-## Hook up the B1500A and call it
+## Hook up the GPIB Interface and call it
+Inter = rm.open_resource('USB0::0x0957::0x0001::0001::INSTR')
+print(Inter.query("*IDN?"))
+
+## Hook up the B1500 and call it
 # explicitly define r/w terminations; set sufficiently large timeout in milliseconds or None.
-B1500A = AgilentB1500("GPIB0::17::INSTR", read_termination='\r\n', write_termination='\r\n', timeout=600000)
-B1500A.initialize_all_smus()
-print(B1500A.id())
+B1500 = AgilentB1500('USB0::0x0957::0x0001::0001::INSTR', read_termination='\r\n', write_termination='\r\n', timeout=60000)
+#B1500 = AgilentB1500('USB0::0x0957::0x0001::0001::INSTR', timeout = 60000)
+B1500.initialize_all_smus()
+B1500.smu1.force('Voltage','Auto Ranging',0.0)

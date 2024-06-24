f1ba38ae24868af7c5ce882561c665b644844721
B1500User <adam.micolich@gmail.com>
2024-06-24 17:17:21 +1200

    Push from B1500 EoD 24Jun24

--- PyNE-kiwi/test.py
+++ PyNE-kiwi/test.py
@@ -7,7 +7,7 @@ Brought to v1.0.0 on Mon Jun 24 2024 by APM
 import pyvisa
 from pymeasure.instruments.agilent import AgilentB1500
 
-## Get the Resource List
+## Establish Resource List
 rm = pyvisa.ResourceManager()
 ResList = rm.list_resources()
 print(ResList)
@@ -16,8 +16,8 @@ print(ResList)
 B2201A = rm.open_resource('GPIB0::22::INSTR')
 print(B2201A.query("*IDN?"))
 
-## Hook up the B1500A by PyMeasure instead
+## Hook up the B1500A and call it
 # explicitly define r/w terminations; set sufficiently large timeout in milliseconds or None.
-B1500A=AgilentB1500("GPIB0::17::INSTR", read_termination='\r\n', write_termination='\r\n', timeout=600000)
-# query SMU config from instrument and initialize all SMU instances
+B1500A = AgilentB1500("GPIB0::17::INSTR", read_termination='\r\n', write_termination='\r\n', timeout=600000)
+B1500A.initialize_all_smus()
 print(B1500A.id())
